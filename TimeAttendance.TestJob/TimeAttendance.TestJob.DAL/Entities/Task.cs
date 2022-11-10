2067a48960ab0b3079c5cf20deb9e567823b8301
Илья Ляпин <IALyapin@stud.kpfu.ru>
2022-11-10 19:57:42 +0300

    Init: Begin settup projject

---
+++ TimeAttendance.TestJob/TimeAttendance.TestJob.DAL/Entities/Task.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeAttendance.TestJob.DAL.Entities
+{
+    public class Task
+    {
+        public Guid Id { get; set; }
+        public string? TaskName { get; set; }
+        public Guid ProjectId { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime CancelDate { get; set; }
+        public DateTime CreateDate { get; set; }
+        public DateTime DeleteDate { get; set; }
+    }
+}
---
+++ TimeAttendance.TestJob/TimeAttendance.TestJob.DAL/Entities/TaskComments.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeAttendance.TestJob.DAL.Entities
+{
+    public class TaskComments
+    {
+        public Guid Id { get; set; }
+        public Guid TaskId { get; set; }
+        public byte CommentType { get; set; }
+        public byte[] Content { get; set; }
+    }
+}
